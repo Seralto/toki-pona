6b468b8bd35bde18821b5d14b764f142f925192d
Sérgio Toledo <contato@sergiotoledo.com.br>
2019-11-26 20:17:20 -0300

    Separate styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
-import {StyleSheet, View, Text, TextInput} from 'react-native';
+import {View, Text, TextInput} from 'react-native';
+
+import styles from './styles/index';
 
 const dic = require('./tokipona-portuguese.json');
 
@@ -36,37 +38,3 @@ export default class App extends Component {
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    display: 'flex',
-    height: '100%',
-    backgroundColor: '#00C14C',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  title: {
-    fontSize: 30,
-    color: '#337ab7',
-  },
-  subtitle: {
-    marginTop: 20,
-    fontSize: 20,
-    color: '#337ab7',
-  },
-  inputText: {
-    backgroundColor: '#fff',
-    fontSize: 20,
-    marginTop: 10,
-    marginBottom: 10,
-    padding: 10,
-    width: 300,
-  },
-  translation: {
-    backgroundColor: '#fff',
-    marginTop: 10,
-    padding: 10,
-    width: 300,
-    fontSize: 20,
-  },
-});
---
+++ src/styles/index.js
@@ -0,0 +1,38 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    height: '100%',
+    // backgroundColor: '#eee',
+    backgroundColor: '#00C14C',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 30,
+    color: '#337ab7',
+  },
+  subtitle: {
+    marginTop: 20,
+    fontSize: 20,
+    color: '#337ab7',
+  },
+  inputText: {
+    backgroundColor: '#fff',
+    fontSize: 20,
+    marginTop: 10,
+    marginBottom: 10,
+    padding: 10,
+    width: 300,
+  },
+  translation: {
+    backgroundColor: '#fff',
+    marginTop: 10,
+    padding: 10,
+    width: 300,
+    fontSize: 20,
+  },
+});
+
+export default styles;
