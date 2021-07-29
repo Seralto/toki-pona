ed9ffa5307a039deb4b344222f4f2eb292c7b8e4
Sérgio Toledo <contato@sergiotoledo.com.br>
2021-07-28 22:51:48 -0300

    Version with side translations

--- App.js
+++ App.js
@@ -1,21 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import {View, Text, TextInput, FlatList} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import styles from './styles/index';
+
+const dic = require('./tokipona-portuguese.json');
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      // translation: '',
+      numWords: 1,
+      words: [],
+    };
+  }
+
+  onTextInputChange(text) {
+    const wordsList = text.trim().split(' ');
+
+    this.setState({
+      words: wordsList
+    });
+
+    this.setState({
+      numWords: wordsList.length
+    });
+  }
+
+  translations() {
+    const words = (word) => {
+      const list = dic[word] || ''
+      if (list === '') {
+        return null
+      }
+
+      const arr = list.split(', ')
+      return (
+        <FlatList
+          data={arr.map(w => ({key: w}))}
+          renderItem={({item, index}) => <Text key={index}>{item.key}</Text>}
+        />
+      )
+    }
+
+    return (
+      this.state.words.map((word, index) => <Text key={index} style={styles.translation}>{words(word)}</Text>)
+    )
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>TokiPona - Português</Text>
+
+        <TextInput
+          onChangeText={text => this.onTextInputChange(text)}
+          value={this.state.input}
+          style={styles.inputText}
+          placeholder="Digite..."
+        />
+
+        <View style={styles.translations}>
+          { this.translations() }
+        </View>
+      </View>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
