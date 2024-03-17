bbf133bfebdf058534f4cb2e473168ef1224e49b
Sérgio Toledo <contato@sergiotoledo.com.br>
2024-03-17 13:10:02 -0300

    Add sitelen pona

--- components/Translation.js
+++ components/Translation.js
@@ -1,12 +1,24 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 
+import { useFonts } from "expo-font";
+
 const Translation = ({ word, text, screenWidth }) => {
   const fontSize = screenWidth < 400 ? 15 : 18;
+  const sitelenPonaFontSize = screenWidth < 400 ? 22 : 26;
+
+  const [loaded] = useFonts({
+    "sitelen-pona": require("../assets/fonts/sitelen-pona.otf"),
+  });
 
   return (
     <View style={styles.translationBox}>
-      <Text style={[styles.word, { fontSize: fontSize }]}>{word}</Text>
+      <View style={styles.wordBox}>
+        <Text style={[styles.word, { fontSize: fontSize }]}>{word}</Text>
+        <Text style={[styles.sitelenPona, { fontSize: sitelenPonaFontSize }]}>
+          {word === "ale/ali" ? "ale" : word}
+        </Text>
+      </View>
       <Text style={[styles.translation, { fontSize: fontSize }]}>{text}</Text>
     </View>
   );
@@ -23,6 +35,17 @@ const styles = StyleSheet.create({
   word: {
     color: "#42455a",
     fontWeight: "bold",
+  },
+  wordBox: {
+    flexDirection: "row",
+    // display: "flex",
+    // alignItems: "center",
+  },
+  sitelenPona: {
+    // marginTop: 5,
+    marginLeft: 5,
+    color: "#42455a",
+    fontFamily: "sitelen-pona",
   },
 });
 
